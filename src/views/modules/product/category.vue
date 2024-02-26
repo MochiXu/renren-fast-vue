3bdcda268ce6c2b2a46b58c699ae88ff1c609558
MochiXu <mochi_xu@icloud.com>
2024-02-26 09:44:09 +0800

    just sync

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,27 @@
 <template>
   <!--  <div>三级分类维护</div>-->
-  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree :data="menus" :props="defaultProps"
+  :expand-on-click-node="false" show-checkbox node-key="catId">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button
+          v-if="node.level<=2"
+          type="text"
+          size="mini"
+          @click="() => append(data)">
+          Append
+        </el-button>
+        <el-button
+          v-if="node.childNodes.length==0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)">
+          Delete
+        </el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 <script>
 </script>
@@ -11,10 +32,10 @@ export default {
   props: {},
   data () {
     return {
-      data: [],
+      menus: [],
       defaultProps: {
         children: 'children',
-        label: 'label'
+        label: 'name'
       }
     }
   },
@@ -29,11 +50,19 @@ export default {
         method: 'get'
       }).then(({data}) => {
         if (data && data.code === 0) {
-          console.log('product category list 获得到菜单数据', data)
+          console.log('product category list 获得到菜单数据', data.data)
+          this.menus = data.data
         } else {
           console.log('product category list 获得菜单数据失败', data)
         }
       })
+    },
+    append (data) {
+      console.log('Try append', data)
+    },
+
+    remove (node, data) {
+      console.log('Try remove', data)
     }
   },
   // 计算属性
